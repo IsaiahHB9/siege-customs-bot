f8702ce82d5d3cd49abaeb6c50216e59ac0effaf
IsaiahHB9 <115671046+IsaiahHB9@users.noreply.github.com>
2026-06-13 03:15:47 -0500

    fix kill role function

--- index.js
+++ index.js
@@ -161,17 +161,19 @@ async function updateStatRoles(member, stats) {
       if (role) await member.roles.add(role).catch(console.error);
     }
   }
-async function updateSingleGameKillRoles(member, kills) {
-  for (const amount in SINGLE_GAME_KILL_ROLES) {
-    if (kills >= Number(amount)) {
-      const role = member.guild.roles.cache.find(r => r.name === SINGLE_GAME_KILL_ROLES[amount]);
+
+  for (const amount in MVP_ROLES) {
+    if (mvpCount >= Number(amount)) {
+      const role = member.guild.roles.cache.find(r => r.name === MVP_ROLES[amount]);
       if (role) await member.roles.add(role).catch(console.error);
     }
   }
 }
-  for (const amount in MVP_ROLES) {
-    if (mvpCount >= Number(amount)) {
-      const role = member.guild.roles.cache.find(r => r.name === MVP_ROLES[amount]);
+
+async function updateSingleGameKillRoles(member, kills) {
+  for (const amount in SINGLE_GAME_KILL_ROLES) {
+    if (kills >= Number(amount)) {
+      const role = member.guild.roles.cache.find(r => r.name === SINGLE_GAME_KILL_ROLES[amount]);
       if (role) await member.roles.add(role).catch(console.error);
     }
   }
